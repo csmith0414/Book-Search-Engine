c8bf65b5b2ee807b24630af2ea802ece880d4adf
Chad Smith <cds021@yahoo.com>
2023-11-29 14:18:55 -0500

    finished resolvers.js file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,7 @@ const resolvers = {
             if (context.user) {
                 return User.findOne({ _id: context.user._id });
             }
-            throw AuthenticationError;
+            throw AuthenticationError('User must log in!');
         },
     },
 
@@ -17,18 +17,52 @@ const resolvers = {
             const user = await User.findOne({ email });
 
             if (!user) {
-                throw AuthenticationError('Incorrect Login');
+                throw AuthenticationError('Incorrect login!');
             }
 
             const correctPw = await user.isCorrectPassword(password);
 
             if (!correctPw) {
-                throw AuthenticationError('Incorrect Login');
+                throw AuthenticationError('Incorrect login!');
             }
 
             const token = signToken(user);
 
             return { token, user };
-        }
-    }
-}
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true },
+                );
+
+                return updatedUser;
+            };
+
+            throw AuthenticationError('User must be logged in to be able to save books to their list!');
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true },
+                );
+                return updatedUser;
+            };
+            throw AuthenticationError('User must be logged in to be able to delete books to their list!');
+        },
+    },
+};
+
+module.exports = resolvers;
